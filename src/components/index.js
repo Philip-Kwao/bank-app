e197bd7d0ceec19609f23b559e955176b9c37495
Philip-Kwao <kwaoizy@gmail.com>
2023-06-30 08:30:31 +0000

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import Business from "./Business";
+import CardDeal from "./CardDeal";
+import Button from "./Button";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedBackCard from "./FeedBackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Navbar,
+    Billing,
+    Business,
+    CardDeal,
+    Button,
+    Clients,
+    CTA,
+    FeedBackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Stats,
+    Testimonials,
+};
